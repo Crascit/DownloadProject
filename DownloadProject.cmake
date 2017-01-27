18e23d264dad78ed2eb53d0cf7c85a36c222bef4
henryiii <henryschreineriii@utexas.edu>
2017-01-27 09:32:09 -0500

    Fix spelling in example
    The example calls this file `DownloadProject` instead of `download_project`. The `.cmake` is not needed. I recommend mentioning that you can add it to your cmake (modules) sub-directory, too.

--- DownloadProject.cmake
+++ DownloadProject.cmake
@@ -73,7 +73,7 @@
 #
 # EXAMPLE USAGE:
 #
-#   include(download_project.cmake)
+#   include(DownloadProject)
 #   download_project(PROJ                googletest
 #                    GIT_REPOSITORY      https://github.com/google/googletest.git
 #                    GIT_TAG             master
